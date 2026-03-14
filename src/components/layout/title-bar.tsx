4187a5a3adc871d7dae9dca8b7356f690a69209d
ACDPDEV <iamacdp.dev@gmail.com>
2026-03-13 23:57:19 -0500

    Move logo to public folder and use it in title bar

--- src/components/layout/title-bar.tsx
+++ src/components/layout/title-bar.tsx
@@ -6,7 +6,7 @@ export default function TitleBar() {
   return (
     <Menubar className="flex pl-3 flex-row h-fit w-screen items-center justify-between rounded-none">
       <div className="flex flex-row gap-4 items-center">
-        <h1 className="font-bold text-sm">Minepack IDE</h1>
+        <img src="/logo.svg" className="w-4 h-4" />
         <MenuOptions />
       </div>
       <WindowActions />
