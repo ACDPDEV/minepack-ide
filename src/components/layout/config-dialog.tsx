e5d108efdba7e9d853cc075ca8b950a0bcade7a9
ACDPDEV <iamacdp.dev@gmail.com>
2026-03-13 23:58:13 -0500

    Add ConfigDialog component with settings UI
    Introduce ConfigButton that opens a dialog with tabs for Project,
    Appearance,
    Editor, Keybindings, and Git, and renders the ProjectTab component.

---
+++ src/components/layout/config-dialog.tsx
@@ -0,0 +1,87 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+import {
+  IconBrush,
+  IconFile,
+  IconFolder,
+  IconGitBranch,
+  IconKeyboard,
+  IconSettings,
+} from "@tabler/icons-react";
+import { Input } from "../ui/input";
+import { useState } from "react";
+import ProjectTab from "./settings/project-tab";
+
+type SettingTab = "project" | "appearance" | "editor" | "keybindings" | "git";
+
+const tabs = [
+  { label: "Project", icon: IconFolder },
+  { label: "Appearance", icon: IconBrush },
+  { label: "Editor", icon: IconFile },
+  { label: "Keybindings", icon: IconKeyboard },
+  { label: "Git", icon: IconGitBranch },
+];
+
+function SettingButton({
+  icon: Icon,
+  label,
+  active,
+  onClick,
+}: {
+  icon: React.ElementType;
+  label: string;
+  active: boolean;
+  onClick: () => void;
+}) {
+  return (
+    <Button
+      variant={active ? "secondary" : "ghost"}
+      className={cn(
+        "text-muted-foreground flex flex-row gap-2 justify-start items-center",
+      )}
+      onClick={active ? () => null : onClick}
+    >
+      <Icon />
+      <span>{label}</span>
+    </Button>
+  );
+}
+
+export function ConfigButton() {
+  const [tab, setTab] = useState<SettingTab>("project");
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={cn("w-12 h-12 rounded-none text-muted-foreground")}
+        >
+          <IconSettings className={cn("stroke-[1.5] scale-150")} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="grid grid-rows-[auto_1fr] max-w-4xl! max-h-8/10! w-full h-full p-0 gap-0">
+        <div className="w-full h-10 flex items-center justify-start px-4">
+          <span>Settings</span>
+        </div>
+        <div className="grid grid-cols-[auto_1fr]">
+          <div className="h-full bg-background border-t border-border p-4 flex flex-col gap-4">
+            <Input />
+            <ul className="flex flex-col">
+              {tabs.map((t) => (
+                <SettingButton
+                  key={t.label}
+                  icon={t.icon}
+                  label={t.label}
+                  active={tab === (t.label.toLowerCase() as SettingTab)}
+                  onClick={() => setTab(t.label.toLowerCase() as SettingTab)}
+                />
+              ))}
+            </ul>
+          </div>
+          <ProjectTab />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
